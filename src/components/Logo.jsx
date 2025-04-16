eb64a31d8807194ee9cbccbac8b082d7e76e9150
ShashankNCoder <shashankngowda8105@gmail.com>
2025-04-16 19:45:46 +0530

    changed the path of logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className="font-['Montserrat'] font-bold text-2xl tracking-tight text-white hover:text-white/90 transition-colors duration-200">
-      <img src="../../../public/logo.svg" alt="Logo" className="h-8 w-9 mr-2 inline-block -mt-1" />
+      <img src="/src/components/logo.svg" alt="Logo" className="h-8 w-9 mr-2 inline-block -mt-1" />
       <span className="text-blue-400">Dev</span>Blog
     </div>
   )
